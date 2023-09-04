5683ea9e58a758e88e955f3ba90639e0f437bc37
mroua <marouf.r.h@gmail.com>
2023-09-04 15:15:54 +0100

    first commit

---
+++ django_pos/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for django_pos project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+    # Index
+    path('', include('pos.urls')),
+    # Products
+    path('products/', include('products.urls')),
+    # Customers
+    path('customers/', include('customers.urls')),
+    # Sales
+    path('sales/', include('sales.urls')),
+]
